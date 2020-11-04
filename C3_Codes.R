a0fad7b43635576c98b027acfd28352d5f681a05
Sureshkumar Venkadesan <sureshkumarv1996@gmail.com>
2020-11-04 12:21:49 +0530

    Add files via upload

---
+++ C3_Codes.R
@@ -0,0 +1,196 @@
+
+
+##Conversion to numeric
+#Interger to numeric
+int_var_name <- as.integer(23)
+class(int_var_name)
+print(int_var_name)
+int_num_var_name <- as.numeric(int_var_name)
+class(int_num_var_name)
+print(int_num_var_name)
+
+#Complex to numeric
+comp_var_name <- 2+3i
+class(comp_var_name)
+print(comp_var_name)
+comp_num_var_name <- as.numeric(comp_var_name)
+class(comp_num_var_name)
+print(comp_num_var_name)
+
+#Logical to numeric
+log_var_name <- TRUE
+class(log_var_name)
+print(log_var_name)
+log_num_var_name <- as.numeric(log_var_name)
+class(log_num_var_name)
+print(log_num_var_name)
+
+#char to numeric
+char_var_name <- "Hello5"
+class(char_var_name)
+print(char_var_name)
+char_num_var_name <- as.numeric(char_var_name)
+class(char_num_var_name)
+print(char_num_var_name)
+
+char_var_name <- "143"
+class(char_var_name)
+print(char_var_name)
+char_num_var_name <- as.numeric(char_var_name)
+class(char_num_var_name)
+print(char_num_var_name)
+
+
+##Conversion to integer
+#Numeric to integer
+num_var_name <- 23.1
+class(num_var_name)
+print(num_var_name)
+num_int_var_name <- as.integer(num_var_name)
+class(num_int_var_name)
+print(num_int_var_name)
+
+#Complex to integer
+comp_var_name <- 2+3i
+class(comp_var_name)
+print(comp_var_name)
+comp_int_var_name <- as.integer(comp_var_name)
+class(comp_int_var_name)
+print(comp_int_var_name)
+
+#Logical to integer
+log_var_name <- TRUE
+class(log_var_name)
+print(log_var_name)
+log_int_var_name <- as.integer(log_var_name)
+class(log_int_var_name)
+print(log_int_var_name)
+
+#char to integer
+char_var_name <- "Hello5"
+class(char_var_name)
+print(char_var_name)
+char_int_var_name <- as.integer(char_var_name)
+class(char_int_var_name)
+print(char_int_var_name)
+
+char_var_name <- "143"
+class(char_var_name)
+print(char_var_name)
+char_int_var_name <- as.integer(char_var_name)
+class(char_int_var_name)
+print(char_int_var_name)
+
+##Conversion to complex
+#Numeric to Complex
+num_var_name <- 23.1
+class(num_var_name)
+print(num_var_name)
+num_comp_var_name <- as.complex(num_var_name)
+class(num_comp_var_name)
+print(num_comp_var_name)
+
+#Integer to Complex
+int_var_name <- as.integer(10)
+class(int_var_name)
+print(int_var_name)
+int_comp_var_name <- as.complex(int_var_name)
+class(int_comp_var_name)
+print(int_comp_var_name)
+
+#Logical to complex
+log_var_name <- TRUE
+class(log_var_name)
+print(log_var_name)
+log_comp_var_name <- as.complex(log_var_name)
+class(log_comp_var_name)
+print(log_comp_var_name)
+
+#char to complex
+char_var_name <- "Hello5"
+class(char_var_name)
+print(char_var_name)
+char_comp_var_name <- as.complex(char_var_name)
+class(char_comp_var_name)
+print(char_comp_var_name)
+
+char_var_name <- "143"
+class(char_var_name)
+print(char_var_name)
+char_comp_var_name <- as.complex(char_var_name)
+class(char_comp_var_name)
+print(char_comp_var_name)
+
+##Conversion to logical
+#Numeric to logical
+num_var_name <- 23.1
+class(num_var_name)
+print(num_var_name)
+num_log_var_name <- as.logical(num_var_name)
+class(num_log_var_name)
+print(num_log_var_name)
+
+#Integer to Logical
+int_var_name <- as.integer(10)
+class(int_var_name)
+print(int_var_name)
+int_log_var_name <- as.logical(int_var_name)
+class(int_log_var_name)
+print(int_log_var_name)
+
+#complex to Logical
+comp_var_name <- 2+1i
+class(comp_var_name)
+print(comp_var_name)
+comp_log_var_name <- as.logical(comp_var_name)
+class(comp_log_var_name)
+print(comp_log_var_name)
+
+#char to Logical
+char_var_name <- "Hello5"
+class(char_var_name)
+print(char_var_name)
+char_log_var_name <- as.logical(char_var_name)
+class(char_log_var_name)
+print(char_log_var_name)
+
+char_var_name <- "143"
+class(char_var_name)
+print(char_var_name)
+char_log_var_name <- as.logical(char_var_name)
+class(char_log_var_name)
+print(char_log_var_name)
+
+##Conversion to Character
+#Numeric to character
+num_var_name <- 23.1
+class(num_var_name)
+print(num_var_name)
+num_char_var_name <- as.character(num_var_name)
+class(num_char_var_name)
+print(num_char_var_name)
+
+#Integer to character
+int_var_name <- as.integer(10)
+class(int_var_name)
+print(int_var_name)
+int_char_var_name <- as.character(int_var_name)
+class(int_char_var_name)
+print(int_char_var_name)
+
+#complex to character
+comp_var_name <- 2+1i
+class(comp_var_name)
+print(comp_var_name)
+comp_char_var_name <- as.character(comp_var_name)
+class(comp_char_var_name)
+print(comp_char_var_name)
+
+#logical to character
+log_var_name <- TRUE
+class(log_var_name)
+print(log_var_name)
+log_char_var_name <- as.character(log_var_name)
+class(log_char_var_name)
+print(log_char_var_name)
+
